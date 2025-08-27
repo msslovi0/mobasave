c0b0b89f45124f19b658c2cb25d2ca8838d390cb
Matthias Slovig <ms@wibros.info>
2025-08-27 15:16:35 +0200

    Login page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home_index', methods: ['GET'])]
+    #[Route('/', name: 'mbs_home', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,13 +2,17 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class LuckyController extends AbstractController
 {
     #[Route('/lucky/number')]
+    #[IsGranted(User::ROLE_USER)]
     public function number(): Response
     {
         $number = random_int(0, 100);
