06e4814147f70e4527dfd6e67928f168db3cd567
Matthias Slovig <matthias@slovig.de>
2025-08-31 19:10:49 +0200

    First list view

--- src/Controller/DatabaseController.php
+++ src/Controller/DatabaseController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Database;
+use App\Entity\Model;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Knp\Component\Pager\PaginatorInterface;
 
 class DatabaseController extends AbstractController
 {
@@ -15,7 +18,7 @@ class DatabaseController extends AbstractController
     {
     }
     #[Route('/collection/{id}', name: 'mbs_database', methods: ['GET'])]
-    public function index(int $id, EntityManagerInterface $entityManager): Response
+    public function index(int $id, EntityManagerInterface $entityManager, PaginatorInterface $paginator, request $request): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $user = $this->security->getUser();
@@ -33,8 +36,16 @@ class DatabaseController extends AbstractController
             $response->setStatusCode(Response::HTTP_FORBIDDEN);
             return $this->render('status/forbidden.html.twig', response: $response);
         }
+        $models = $entityManager->getRepository(Model::class)->findBy(["modeldatabase" => $database]);
+
+        $pagination = $paginator->paginate(
+            $models,
+            $request->query->getInt('page', 1), /* page number */
+            100 /* limit per page */
+        );
+
         return $this->render('collection/list.html.twig', [
-            "database" => $database
+            "models" => $pagination
         ]);
     }
 }
